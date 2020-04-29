b295c801909126bd3089f34adb6b40fad0e0cdf0
LostLite <williammuriuki@gmail.com>
2020-04-29 16:08:52 +0300

    first commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+load_dotenv()
+
+app = Flask(__name__)
+# Set secret key for our application to protect against Cross-site requests and forgery attacks
+app.config['SECRET_KEY'] = '9260949052f68579573152b494295610'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv("mail_username")
+app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
+mail = Mail(app)
+
+
+from flaskblog import routes
